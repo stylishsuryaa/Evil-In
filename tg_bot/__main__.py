55ca63b6d15aae294a5949fb5686d52146058634
stylishsuryaa <83962899+stylishsuryaa@users.noreply.github.com>
2022-04-26 05:05:21 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -28,7 +28,7 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 
 PM_START_TEXT = """
-*𝙷𝚢 {} , 𝙼𝚢 𝙽𝚊𝚖𝚎 𝚒𝚜 {}!*
+*𝙷i {} , 𝙼𝚢 𝙽𝚊𝚖𝚎 𝚒𝚜 {}!*
 
 𝙸 𝚊𝚖 𝚊𝚗 *𝚂𝚄𝙿𝙴𝚁*  𝚐𝚛𝚘𝚞𝚙 𝚖𝚊𝚗𝚊𝚐𝚎𝚖𝚎𝚗𝚝 𝚋𝚘𝚝.
 
@@ -53,7 +53,7 @@ HELP_STRINGS = """
 𝙰𝚗𝚍 𝚝𝚑𝚎 𝚏𝚘𝚕𝚕𝚘𝚠𝚒𝚗𝚐:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\n𝙰𝚕𝚕 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝚌𝚊𝚗 𝚎𝚒𝚝𝚑𝚎𝚛 𝚋𝚎 𝚞𝚜𝚎𝚍 𝚠𝚒𝚝𝚑 / 𝚘𝚛 !.\n")
 
-TECHNO_IMG = "https://telegra.ph/file/10cf6a74f348caa0096b9.jpg"
+TECHNO_IMG = "https://telegra.ph/file/dd120b7915edf85703655.jpg"
 IMPORTED = {}
 MIGRATEABLE = []
 HELPABLE = {}
